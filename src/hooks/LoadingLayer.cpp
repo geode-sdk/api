e2f1429249360acf1c891e490db242f0bffc1f4e
HJfod <60038575+HJfod@users.noreply.github.com>
2022-02-04 13:48:34 +0200

    update loadinglayer and fix spelling issue in mod.json

--- src/hooks/LoadingLayer.cpp
+++ src/hooks/LoadingLayer.cpp
@@ -1,42 +1,16 @@
 #include "hook.hpp"
 #include <array>
 
-static std::string loadStepToString(int step) {
-    if (step < 1) {
-        return "GJ_GameSheet";
-    }
-    if (step >= 14) {
-        return "Finished";
-    }
-    std::array<const char*, 14> steps = {
-        "GJ_GameSheet02",
-        "GJ_GameSheet03",
-        "GJ_GameSheet04",
-        "GJ_GameSheetGlow",
-        "FireSheet_01, GJ_ShopSheet, smallDot, square02_001",
-        "CCControlColourPickerSpriteSheet, GJ_gradientBG, edit_barBG_001, GJ_button_01, slidergroove2, sliderBar2",
-        "GJ_square01, GJ_square02, GJ_square03, GJ_square04, GJ_square05, gravityLine_001",
-        "chatFont",
-        "Robot_AnimDesc, glassDestroy01, coinPickupEffect, explodeEffect",
-        "nothing lol",
-        "GameManager",
-        "CCTextInputNode",
-        "CCCircleWave",
-        "Finished",
-    };
-    return steps[step - 1];
-}
-
-class $modify(LoadingLayer) {
+class $modify(CustomLoadingLayer, LoadingLayer) {
     bool init(bool fromReload) {
         if (!$LoadingLayer::init(fromReload))
             return false;
 
         auto winSize = CCDirector::sharedDirector()->getWinSize();
-        
+
         auto [count, unresolvedCount] = Loader::get()->getLoadedModCount();
 
-        const char* text = "test";
+        const char* text = "Loaded Geode";
         // commented out until someone figures out why this crashes
         // const char* text = unresolvedCount ?
         //     CCString::createWithFormat("Geode: Loaded %zu mods (%zu unresolved)", count, unresolvedCount)->getCString() : 
@@ -50,12 +24,4 @@ class $modify(LoadingLayer) {
 
         return true;
     }
-
-	void loadAssets() {
-		$LoadingLayer::loadAssets();
-
-        auto str = "Loading " + loadStepToString(this->m_loadStep);
-        auto idk = this->getChildByTag(5);
-        as<CCLabelBMFont*>(this->getChildByTag(5))->setString(str.c_str());
-	}
 };
