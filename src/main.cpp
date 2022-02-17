00614f180ec1f4f453ef71a58cbef566798d8261
FigmentBoy <17240592+FigmentBoy@users.noreply.github.com>
2022-02-17 11:31:10 -0600

    Overwrite files

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ GEODE_API bool GEODE_CALL geode_load(Mod* mod) {
     mod->with<GeodeAPI>()->addDragDropHandler("geode_mod_installer", [](ghc::filesystem::path path) -> bool {
         auto to_file = Loader::get()->getGeodeDirectory() / geode_mod_directory / path.filename();
 
-        if (!ghc::filesystem::copy_file(path, to_file)) {
+        if (!ghc::filesystem::copy_file(path, to_file, ghc::filesystem::copy_options::overwrite_existing)) {
             ghc::filesystem::remove(path);
             FLAlertLayer::create("Oops!", path.stem().u8string() + " couldn't be installed!", "OK")->show();
         } else {
