7bf2b9421b22f091cf38fb061933c7d3adc304be
FigmentBoy <17240592+FigmentBoy@users.noreply.github.com>
2022-02-17 20:14:42 -0600

    crash fix, will reimplement later

--- src/hooks/LoadingLayer.cpp
+++ src/hooks/LoadingLayer.cpp
@@ -1,27 +1,27 @@
 #include "hook.hpp"
 #include <array>
 
-class $modify(CustomLoadingLayer, LoadingLayer) {
-    bool init(bool fromReload) {
-        if (!LoadingLayer::init(fromReload))
-            return false;
+// class $modify(CustomLoadingLayer, LoadingLayer) {
+//     bool init(bool fromReload) {
+//         if (!LoadingLayer::init(fromReload))
+//             return false;
 
-        auto winSize = CCDirector::sharedDirector()->getWinSize();
+//         auto winSize = CCDirector::sharedDirector()->getWinSize();
 
-        auto [count, unresolvedCount] = Loader::get()->getLoadedModCount();
+//         auto [count, unresolvedCount] = Loader::get()->getLoadedModCount();
 
-        const char* text = "Loaded Geode";
-        // commented out until someone figures out why this crashes
-        // const char* text = unresolvedCount ?
-        //     CCString::createWithFormat("Geode: Loaded %zu mods (%zu unresolved)", count, unresolvedCount)->getCString() : 
-        //     CCString::createWithFormat("Geode: Loaded %zu mods", count)->getCString();
+//         const char* text = "Loaded Geode";
+//         // commented out until someone figures out why this crashes
+//         // const char* text = unresolvedCount ?
+//         //     CCString::createWithFormat("Geode: Loaded %zu mods (%zu unresolved)", count, unresolvedCount)->getCString() : 
+//         //     CCString::createWithFormat("Geode: Loaded %zu mods", count)->getCString();
 
-        auto label = CCLabelBMFont::create(text, "goldFont.fnt");
-        label->setPosition(winSize.width / 2, 30.f);
-        label->setScale(.45f);
-        label->setTag(5);
-        this->addChild(label);
+//         auto label = CCLabelBMFont::create(text, "goldFont.fnt");
+//         label->setPosition(winSize.width / 2, 30.f);
+//         label->setScale(.45f);
+//         label->setTag(5);
+//         this->addChild(label);
 
-        return true;
-    }
-};
+//         return true;
+//     }
+// };
