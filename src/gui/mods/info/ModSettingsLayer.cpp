3ca852135d2c3ff66ef8f8184c8545e74d5e5a82
HJfod <60038575+HJfod@users.noreply.github.com>
2022-04-10 14:51:08 +0600

    add settings button to gui + create ScrollLayer class for easier scrolling (which doesn't work yet tho and crashes for some reason)

--- src/gui/mods/info/ModSettingsLayer.cpp
+++ src/gui/mods/info/ModSettingsLayer.cpp
@@ -2,36 +2,35 @@
 #include "../settings/ModSettingsListView.hpp"
 
 bool ModSettingsLayer::init(Mod* mod) {
-    this->m_noElasticity = true;
-
-    this->m_mod = mod;
+    m_noElasticity = true;
+    m_mod = mod;
 
     auto winSize = CCDirector::sharedDirector()->getWinSize();
 	CCSize size { 420.f, 280.f };
 
     if (!this->initWithColor({ 0, 0, 0, 105 })) return false;
-    this->m_mainLayer = CCLayer::create();
-    this->addChild(this->m_mainLayer);
+    m_mainLayer = CCLayer::create();
+    this->addChild(m_mainLayer);
 
     auto bg = CCScale9Sprite::create("GJ_square02.png", { 0.0f, 0.0f, 80.0f, 80.0f });
     bg->setContentSize(size);
     bg->setPosition(winSize.width / 2, winSize.height / 2);
-    this->m_mainLayer->addChild(bg);
+    m_mainLayer->addChild(bg);
 
-    this->m_buttonMenu = CCMenu::create();
-    this->m_mainLayer->addChild(this->m_buttonMenu);
+    m_buttonMenu = CCMenu::create();
+    m_mainLayer->addChild(m_buttonMenu);
 
-	auto nameStr = this->m_mod->getName() + " Settings";
+	auto nameStr = m_mod->getName() + " Settings";
     auto nameLabel = CCLabelBMFont::create(
         nameStr.c_str(), "bigFont.fnt"
     );
     nameLabel->setPosition(winSize.width / 2, winSize.height / 2 + 110.f);
     nameLabel->setScale(.7f);
-    this->m_mainLayer->addChild(nameLabel, 2); 
+    m_mainLayer->addChild(nameLabel, 2); 
 
 	auto settings = ModSettingsListView::create(mod, 300.f, 200.f);
 	settings->setPosition(winSize.width / 2 - 150.f, winSize.height / 2 - 100.f);
-	this->m_mainLayer->addChild(settings);
+	m_mainLayer->addChild(settings);
 
     CCDirector::sharedDirector()->getTouchDispatcher()->incrementForcePrio(2);
     this->registerWithTouchDispatcher();
@@ -44,11 +43,8 @@ bool ModSettingsLayer::init(Mod* mod) {
         this,
         (SEL_MenuHandler)&ModSettingsLayer::onClose
     );
-    closeBtn->setUserData(reinterpret_cast<void*>(this));
-
-    this->m_buttonMenu->addChild(closeBtn);
-
-    closeBtn->setPosition( - size.width / 2, size.height / 2 );
+    closeBtn->setPosition(-size.width / 2, size.height / 2 );
+    m_buttonMenu->addChild(closeBtn);
 
     this->setKeypadEnabled(true);
     this->setTouchEnabled(true);
@@ -58,7 +54,7 @@ bool ModSettingsLayer::init(Mod* mod) {
 
 void ModSettingsLayer::keyDown(enumKeyCodes key) {
     if (key == KEY_Escape)
-        return onClose(nullptr);
+        return this->onClose(nullptr);
     if (key == KEY_Space)
         return;
     return FLAlertLayer::keyDown(key);
--- src/gui/mods/settings/ModSettingsListView.hpp
+++ src/gui/mods/settings/ModSettingsListView.hpp
@@ -5,17 +5,17 @@
 USE_GEODE_NAMESPACE();
 
 class ModSettingsListView : public CCScrollLayerExt {
-    protected:
-        Mod* m_mod;
+protected:
+    Mod* m_mod;
 
-        bool init(Mod* mod, float width, float height);
+    bool init(Mod* mod, float width, float height);
 
-        ModSettingsListView(CCRect const& rect);
+    ModSettingsListView(CCRect const& rect);
 
-        void scrollWheel(float y, float x) override;
+    void scrollWheel(float y, float x) override;
 
-    public:
-        static ModSettingsListView* create(
-            Mod* Mod, float width, float height
-        );
+public:
+    static ModSettingsListView* create(
+        Mod* Mod, float width, float height
+    );
 };
